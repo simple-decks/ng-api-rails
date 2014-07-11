5a13e76554ff2554a8b414d27c4424547b69c1b7
Anders Ramsay <andersr@gmail.com>
2014-07-11 19:03:47 -0400

    add user resource, take 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :names, except: [:new, :edit]
+  resources :users, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
