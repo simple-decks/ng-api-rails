cca75487f7b124227209843a8e64111f2ed5e780
Anders Ramsay <andersr@gmail.com>
2014-07-11 19:04:43 -0400

    add session resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711225041) do
+ActiveRecord::Schema.define(version: 20140711230407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "names", force: true do |t|
+    t.string   "twitter_name"
+    t.string   "gravatar"
+    t.string   "provider"
+    t.string   "email"
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sessions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
     t.string   "twitter_name"
     t.string   "gravatar"
     t.string   "provider"
